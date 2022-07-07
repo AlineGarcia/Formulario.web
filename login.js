463016439360095f84e2e8db07302f4ea67e2489
AlineGarcia <alinecarinagarcia@hotmail.com>
2022-07-07 20:57:35 -0300

    tela de cadastro

--- login.js
+++ login.js
@@ -1,15 +1,27 @@
 var login = document.querySelector('#login')
-    var senha = document.querySelector('#senha')
-    var botao = document.querySelector('#botao')
-    var msg = document.querySelector('#msg')
+var senha = document.querySelector('#senha')
+var botao = document.querySelector('#botao')
+var msg = document.querySelector('#msg')
+var btnCadastro = document.querySelector('#btnCadastro')
+var btnVoltar = document.querySelector('#btnVoltar')
 
-    botao.addEventListener('click', e => {
-        console.log('login:', login.value)
-        console.log('senha:', senha.value)
-        
-        if (login.value == 'aline2000' && senha.value == 'lininha') {
-            window.location.href = "https://outlook.live.com/mail/0/"
-        } else {
-            msg.innerHTML = 'login ou senha inválida'
-        }   
-    }) 
+
+botao.addEventListener('click', e => {
+    console.log('login:', login.value)
+    console.log('senha:', senha.value)
+    
+    if (login.value == 'aline2000' && senha.value == 'lininha') {
+        window.location.href = "https://outlook.live.com/mail/0/"
+    } else {
+        msg.innerHTML = 'login ou senha inválida'
+    }   
+}) 
+
+
+btnCadastro.addEventListener('click', e => {
+    window.location.href = "./cadastro.html"
+})
+
+btnVoltar.addEventListener('click', e => {
+    window.location.href = "./index.html"
+})
